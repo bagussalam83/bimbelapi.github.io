295414b139062151da3560e859255a5ff58a9b3e
bagussalam83 <salambagus.bs@gmail.com>
2023-03-05 19:48:48 +0700

    page-home-done

---
+++ js/index.js
@@ -0,0 +1,88 @@
+// modal 1
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("img-pelajaran")) {
+    const src = e.target.getAttribute("src");
+    document.querySelector(".modal-img").src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById("mata-pelajaran-1")
+    );
+    myModal.show();
+  }
+});
+// modal 2
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("img-pelajaran-2")) {
+    const src = e.target.getAttribute("src");
+    document.querySelector(".modal-img-2").src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById("mata-pelajaran-2")
+    );
+    myModal.show();
+  }
+});
+// modal 3
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("img-pelajaran-3")) {
+    const src = e.target.getAttribute("src");
+    document.querySelector(".modal-img-3").src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById("mata-pelajaran-3")
+    );
+    myModal.show();
+  }
+});
+// modal 4
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("img-pelajaran-4")) {
+    const src = e.target.getAttribute("src");
+    document.querySelector(".modal-img-4").src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById("mata-pelajaran-4")
+    );
+    myModal.show();
+  }
+});
+// modal 5
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("img-pelajaran-5")) {
+    const src = e.target.getAttribute("src");
+    document.querySelector(".modal-img-5").src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById("mata-pelajaran-5")
+    );
+    myModal.show();
+  }
+});
+// modal 6
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("img-pelajaran-6")) {
+    const src = e.target.getAttribute("src");
+    document.querySelector(".modal-img-6").src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById("mata-pelajaran-6")
+    );
+    myModal.show();
+  }
+});
+// modal 7
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("img-pelajaran-7")) {
+    const src = e.target.getAttribute("src");
+    document.querySelector(".modal-img-7").src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById("mata-pelajaran-7")
+    );
+    myModal.show();
+  }
+});
+// modal 8
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("img-pelajaran-8")) {
+    const src = e.target.getAttribute("src");
+    document.querySelector(".modal-img-8").src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById("mata-pelajaran-8")
+    );
+    myModal.show();
+  }
+});
